6d88339c78a7b7dfc3056a7df01ffc36e76b94bf
Thomas Angelo <tangelo@axs.com>
2019-02-28 18:16:01 -0800

    Adding Unit Tests
    Some Unit Testing

---
+++ MasterMindTests/Controllers/CodeBreakerTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasterMind.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind.Controllers.Tests
+{
+    [TestClass()]
+    public class CodeBreakerTests
+    {
+
+        [TestMethod()]
+        public void SolveCodeTooManyCharacters()
+        {
+            string codeGuess = "123456";
+            string codeAnswer = "1234";
+            var codeBreaker = new CodeBreaker();
+
+            var response = codeBreaker.SolveCode(codeGuess, codeAnswer);
+
+            Assert.AreEqual(response, "Please enter a 4 digit number with each digit betweemn 1 and 6.");
+        }
+
+        [TestMethod()]
+        public void SolveCodeNonNumericCharacters()
+        {
+            string codeGuess = "12hd";
+            string codeAnswer = "1234";
+            var codeBreaker = new CodeBreaker();
+
+            var response = codeBreaker.SolveCode(codeGuess, codeAnswer);
+
+            Assert.AreEqual(response, "Please enter a 4 digit number with each digit betweemn 1 and 6.");
+        }
+
+        [TestMethod()]
+        public void SolveCodeTooFewCharacters()
+        {
+            string codeGuess = "12";
+            string codeAnswer = "1234";
+            var codeBreaker = new CodeBreaker();
+
+            var response = codeBreaker.SolveCode(codeGuess, codeAnswer);
+
+            Assert.AreEqual(response, "Please enter a 4 digit number with each digit betweemn 1 and 6.");
+        }
+
+        [TestMethod()]
+        public void SolveCodeCharacterValuesTooHighOrLow()
+        {
+            string codeGuess = "0173";
+            string codeAnswer = "1234";
+            var codeBreaker = new CodeBreaker();
+
+            var response = codeBreaker.SolveCode(codeGuess, codeAnswer);
+
+            Assert.AreEqual(response, "Please enter a 4 digit number with each digit betweemn 1 and 6.");
+        }
+
+        [TestMethod()]
+        public void SolveCodeWorking()
+        {
+            string codeGuess = "1253";
+            string codeAnswer = "1234";
+            var codeBreaker = new CodeBreaker();
+
+            var response = codeBreaker.SolveCode(codeGuess, codeAnswer);
+
+            Assert.AreEqual(response, "++-" + System.Environment.NewLine + "You have 9 attempts remaining.");
+        }
+
+        [TestMethod()]
+        public void SolveCodeYouWin()
+        {
+            string codeGuess = "1234";
+            string codeAnswer = "1234";
+            var codeBreaker = new CodeBreaker();
+
+            var response = codeBreaker.SolveCode(codeGuess, codeAnswer);
+
+            Assert.AreEqual(response, "Congratulations! You win. You are a true MasterMind!");
+        }
+    }
+}
---
+++ MasterMindTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MasterMindTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MasterMindTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d725273f-5e24-48dd-ac4b-58c6a601d6bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
